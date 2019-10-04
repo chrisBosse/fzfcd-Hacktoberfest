4bd7b7a9074b1a1860274f02a58f728890c03c49
Chris Bosse <30784811+chrisBosse@users.noreply.github.com>
2019-10-04 07:22:45 -0400

    Make script executable

--- fzfcd.sh
+++ fzfcd.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # copy the content of this file to your .bashrc file or use the 'source' command in it.
 # TODO: find a new alias fzfind that finds the location of a file and changes the directory to there.
 # TODO: modify fzfcd and fzfind so that the command also looks for useful hidden files such as .config/* or .bashrc but not .git/*
