a8eeeebc6f0ff5ed3ab36ace334d19c54a9869ee
Chris Bosse <30784811+chrisBosse@users.noreply.github.com>
2019-10-04 07:23:39 -0400

    Add more instructions

--- fzfcd.sh
+++ fzfcd.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
-# copy the content of this file to your .bashrc file or use the 'source' command in it.
+# To use:
+#  - Append the lines below # CONTENT to '~/.bashrc' file.
+#  - Then logout and re-login.  Or use 'source ~/.bashrc' to load the changes.
+#
+# One-time, non-persistent, use:
+#  - Use 'source ./fzfcs.sh' for the shell session.
+
 # TODO: find a new alias fzfind that finds the location of a file and changes the directory to there.
 # TODO: modify fzfcd and fzfind so that the command also looks for useful hidden files such as .config/* or .bashrc but not .git/*
 
+# CONTENT
 alias fzfcd='cd "$(find "$HOME"/* -type d | fzf -e)"'
